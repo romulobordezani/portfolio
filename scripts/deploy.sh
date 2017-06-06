008412f6fe9d4c2b4b0a86ecb61eff3c18c268b5
travis <romulobordezani@gmail.com>
2017-06-06 18:31:18 -0300

    Travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,12 +9,10 @@ if [ $TRAVIS_BRANCH == "master" ] ; then
     eval "$(ssh-agent -s)"
     ssh-add ~/.ssh/travis_rsa
     git remote add deploy "travis@romulobordezani.com.br:/var/www/romulobordezani.com.br"
-    git config user.name "Travis CI"
+    git config user.name "travis"
     git config user.email "romulobordezani@gmail.com"
 
     # commit compressed files and push it to remote
-    rm -f .gitignore
-    cp .travis/deployignore .gitignore
     git add .
     git status # debug
     git commit -m "Deploy compressed files"
