0f9f14d1476d96408b922c909c33acd7184f6562
romulo bordezani <romulobordezani@gmail.com>
2017-06-06 17:29:35 -0300

    Ddding Travis Deploy

---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# print outputs and exit on first failure
+set -xe
+
+if [ $TRAVIS_BRANCH == "master" ] ; then
+
+    # setup ssh agent, git config and remote
+    eval "$(ssh-agent -s)"
+    ssh-add ~/.ssh/travis_rsa
+    git remote add deploy "travis@romulobordezani.com.br:/var/www/romulobordezani.com.br"
+    git config user.name "Travis CI"
+    git config user.email "romulobordezani@gmail.com"
+
+    # commit compressed files and push it to remote
+    rm -f .gitignore
+    cp .travis/deployignore .gitignore
+    git add .
+    git status # debug
+    git commit -m "Deploy compressed files"
+    git push -f deploy HEAD:master
+
+else
+
+    echo "No deploy script for branch '$TRAVIS_BRANCH'"
+
+fi
