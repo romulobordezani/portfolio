3f535808134ec52eaf5f04ccdf180147e7382f2f
Romulo Bordezani <romulobordezani@gmail.com>
2021-01-06 04:22:14 -0300

    chore(clean-up-): preparing for next.js

--- app/scripts/filters/randomize.filter.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('rbApp').filter('randomize', function() {
-    return function(input) {
-        if ( input!==null && input!==undefined && input > 1) {
-            return Math.floor((Math.random()*input)+1);
-        }
-    };
-});
