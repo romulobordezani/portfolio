bf16c8c978f5aa11c05bc529b9836c27b49f07e2
romulobordezani <romulo.bordezani@picpay.com>
2022-06-03 00:41:30 -0300

    refactor/next.js-version

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
+
 const nextConfig = {
   reactStrictMode: true,
   basePath: '/portfolio',
-}
+};
 
-module.exports = nextConfig;
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }]
+], nextConfig);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import SvgDAPorra from '../public/vercel.svg';
 
 const Home: NextPage = () => {
   return (
@@ -61,7 +61,7 @@ const Home: NextPage = () => {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img src={SvgDAPorra} alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
