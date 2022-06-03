858d20d0dac5be301250fcbd2faeac2c27efdbf2
romulobordezani <romulo.bordezani@picpay.com>
2022-06-03 01:00:59 -0300

    fix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: '/portfolio',
+  reactStrictMode: true/*,
+  basePath: '/portfolio',*/
 };
 
 module.exports = withPlugins([
