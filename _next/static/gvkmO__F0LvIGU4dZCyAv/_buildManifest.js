503311b01c501a6730a16749aa9167846d694c83
romulobordezani <romulo.bordezani@picpay.com>
2022-06-06 04:45:44 -0300

    feat(sunrise)

--- _next/static/gvkmO__F0LvIGU4dZCyAv/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-c88538e2575410d6.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],"/about":["static/chunks/pages/about-522809d68fd61135.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
